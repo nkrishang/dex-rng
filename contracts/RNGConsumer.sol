0a41baded2b74f29bea51d5031b61be1f3a98404
nkrishang <kn1457@nyu.edu>
2021-06-16 20:04:27 -0400

    rename to dex rng and add bounty text

--- contracts/RNGConsumer.sol
+++ contracts/RNGConsumer.sol
@@ -4,30 +4,36 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import './IDexRNG.sol';
 
+/**
+ * This contract is meant to be used for the DEX RNG bounty by NFT Labs.
+ * You can find the bounty at - https://gitcoin.com/xxx
+ * DEX RNG address - 0x0000000000000000000000000000000000000000
+ */
+
 contract RNGConsumer is Ownable {
 
   uint public range = 100;
   uint public randomNumber;
   bool public acceptableEntropy;
 
-  IDexRNG internal priceRNG;
+  IDexRNG internal dexRNG;
 
   event RandomNumber(uint randomNumber);
   event Success(address bountyHunter, uint randomNumber);
   event TryAgain(address bountyHunter, uint randomNumber);
 
-  constructor(address _priceRNG) {
-    priceRNG = IDexRNG(_priceRNG);
+  constructor(address _dexRNG) {
+    dexRNG = IDexRNG(_dexRNG);
   }
 
-  /// @dev Sets the address of the Price RNG.
-  function setPriceRNG(address _priceRNG) external onlyOwner {
-    priceRNG = IDexRNG(_priceRNG);
+  /// @dev Sets the address of the DEX RNG.
+  function setdexRNG(address _dexRNG) external onlyOwner {
+    dexRNG = IDexRNG(_dexRNG);
   }
 
-  /// @dev Gets a random number from the Price RNG.
+  /// @dev Gets a random number from the DEX RNG.
   function random(uint predictedRandomNumber) external {
-    (randomNumber, acceptableEntropy) = priceRNG.getRandomNumber(range);
+    (randomNumber, acceptableEntropy) = dexRNG.getRandomNumber(range);
 
     if(predictedRandomNumber == randomNumber && acceptableEntropy) {
       emit Success(msg.sender, randomNumber);
